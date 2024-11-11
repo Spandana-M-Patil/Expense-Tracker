9f2c6326ee52dc0390eb6f80f53cb9df76aa1bd3
spandana <spandanampatil2004@gmail.com>
2024-11-11 14:31:27 +0530

    Expenses can be added

--- ExpenseTrackApp/admin.py
+++ ExpenseTrackApp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ExpenseAdd as Expenses
 
 # Register your models here.
+admin.site.register(Expenses)
--- ExpenseTrackApp/views.py
+++ ExpenseTrackApp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ExpenseForm
+from django.http import HttpResponse
+from datetime import date
 
 # Create your views here.
+def expenseAddView(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            print("Form errors:", form.errors)  # Shows any validation issues
+    else:
+        form = ExpenseForm()
+
+    return render(request, 'dashboard.html', {'form': form})
